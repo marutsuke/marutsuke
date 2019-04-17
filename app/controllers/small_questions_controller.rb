7814e35b4f9af2350767538ea0bee5f029b3baf6
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-04-17 19:23:35 +0900

    before_migration

--- app/controllers/small_questions_controller.rb
+++ app/controllers/small_questions_controller.rb
@@ -1,5 +1,7 @@
 class SmallQuestionsController < ApplicationController
+
   def show
+    set_small_question
 
   end
 
@@ -18,4 +20,9 @@ class SmallQuestionsController < ApplicationController
   def update
 
   end
+
+  def set_small_question
+    @small_question = SmallQuestion.find(params[:smallquestionid])
+  end
+
 end
---
+++ db/migrate/20190417102253_small_questions.rb
@@ -0,0 +1,4 @@
+class SmallQuestions < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
