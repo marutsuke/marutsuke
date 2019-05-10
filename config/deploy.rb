b0fa3881a6f3e965c8ce5bc85c40ecea1ffadaa0
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-05-10 20:59:42 +0900

    Commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,8 @@ set :keep_releases, 5
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: Rails.application.credentials.aws[:access_key_id],
-  AWS_SECRET_ACCESS_KEY: Rails.application.credentials.aws[:secret_access_key]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
 after 'deploy:publishing', 'deploy:restart'
