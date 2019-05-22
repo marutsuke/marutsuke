c0fc1f78710bd04119fa7a5f4e3de6b28d358b46
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-05-22 21:11:01 +0900

    Commit

--- app/controllers/small_questions_controller.rb
+++ app/controllers/small_questions_controller.rb
@@ -7,17 +7,7 @@ class SmallQuestionsController < ApplicationController
   end
 
   def show
-    @small_question = SmallQuestion.find(params[:smallquestionid])
-    @question = @small_question.question
-    unless @small_question.answers.where(answer:params[:answer]) == []
-      count = @small_question.correct_count + 1
-      @small_question.update(correct_count:count)
-      respond_to do |format|
-        format.html{
-        }
-        format.json
-      end
-    end
+
   end
 
   def new
