a186b2d5c9f5959fbc1968f345e639194434d61d
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-04-29 15:03:43 +0900

    test: リクエストスペックを作成

---
+++ spec/requests/admin/admins_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe Admin::AdminsController, type: :request do
+  describe '#new /admin/admins' do
+      it 'createエラー後更新してもアクセスできる' do
+      get '/admin/admins'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe '#new /admin/admins/new' do
+    it 'アクセスできる' do
+      get new_admin_admin_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe '#create /admin/admins/new' do
+    let(:admin_params){ {
+        name: "テスト",
+        email: "test_mail@test.com",
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    }
+    it '管理者を作成できる' do
+      expect {
+        post admin_admins_path, params: { admin: admin_params }
+      }.to change(Admin, :count).by(1)
+      expect(response).to have_http_status 302
+      expect(response).to redirect_to admin_users_path
+    end
+
+    context 'パスワード確認を間違えた時' do
+      before { admin_params[:password] = 'fail_password' }
+
+      it '管理者を作成に失敗したらエラーがでる' do
+        expect {
+          post admin_admins_path, params: { admin: admin_params }
+        }.to change(Admin, :count).by(0)
+        expect(response).to have_http_status 200
+        expect(body).to include('パスワード(確認)と入力が一致しません。')
+      end
+    end
+  end
+end
