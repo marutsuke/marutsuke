0363d64d2bb4b29d4ca74bfb70d9229f1bef99f2
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-04-29 15:27:39 +0900

    feat: hamlに変換、デバッグ機能追加

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,12 @@ class Admin::SessionsController < ApplicationController
   end
 
   def create
+    @admin = Admin.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'メールアドレスまたはパスワードが間違っています。'
+      render :new
+    end
   end
 
   def destroy
