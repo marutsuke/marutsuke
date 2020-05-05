b564cea58bc0bf9b0c519622f8c104f0d0469d40
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-05-05 17:25:22 +0900

    feat: メールアドレスでもログインIDでもログインを自然にできるように

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(login_id: session_params[:login_id])
+    user = search_user_from_email_or_login_id
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       login_count_up(user)
       redirect_to root_path, notice: "#{user.name}さん、こんにちは！"
     else
-      render :new, notice: 'ログインに失敗しました。'
+      flash.now[:danger] = if session_params[:email].present?
+                             'メールアドレスまたはパスワードが間違っています'
+                           else
+                             'ログインIDまたはパスワードが間違っています'
+                           end
+      render :new
     end
   end
 
@@ -24,11 +29,19 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:login_id, :password)
+    params.require(:session).permit(:login_id, :password, :email)
   end
 
   def login_count_up(user)
     login_count = user.login_count + 1
     user.update_attribute(:login_count, login_count)
+  end
+
+  def search_user_from_email_or_login_id
+    if session_params[:login_id].present?
+      User.find_by(login_id: session_params[:login_id])
+    elsif session_params[:email].present?
+      User.find_by(email: session_params[:email])
+    end
   end
 end
