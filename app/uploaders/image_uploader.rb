a2e573a74e6a295795d7259ff7b6664e822c3450
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-05-10 10:33:22 +0900

    feat: 画像サイズが小さすぎたので、もう少し大きく調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-  process resize_to_limit: [800, 800]
+  process resize_to_limit: [900, 600]
 
   # Process files as they are uploaded:
   #
