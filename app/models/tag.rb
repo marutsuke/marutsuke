0fdb44eab55af8f4256eb7ebd5acee6a6929e279
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-07 11:39:29 +0900

    Feat: 学校が複数のタグを持つ実装を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
-  validates :name, :school_id, presence: true
+  validates :name, :school_id, presence: true, length: { maximum: 10 }
   validates :name, uniqueness: { scope: :school_id }
   has_many :user_tags
   has_many :lesson_tags
