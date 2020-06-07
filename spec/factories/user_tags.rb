1f6b948791392cf13c198c4270bb3aec0a590d23
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-07 10:36:21 +0900

    feat: tagとlessonとuserの関係をspecでテスト

--- spec/factories/user_tags.rb
+++ spec/factories/user_tags.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user_tag do
-    
+    association :school, factory: [:user]
+    association :school, factory: [:tag]
   end
 end
--- spec/models/lesson_tag_spec.rb
+++ spec/models/lesson_tag_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe LessonTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:lesson) }
+    it { is_expected.to belong_to(:tag) }
+  end
 end
