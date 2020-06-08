0c614f0afa81401575cb18b3ff3d8bb12ad90124
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-08 22:38:52 +0900

    Feat: とりあえず、ユーザーにタグを登録できるように

--- app/controllers/teacher/user_tags_controller.rb
+++ app/controllers/teacher/user_tags_controller.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 class Teacher::UserTagsController < ApplicationController
+  def create
+    @user_tag = UserTag.new(user_tag_params)
+    respond_to do |format|
+      if @user_tag.save && current_school.users.find_by(id: @user_tag.user.id)
+        format.js # create.js.erb
+      else
+        flash[:danger] = 'タグ登録に失敗しました。もう一度試してください。解決しない場合は、お問い合わせください。'
+        redirect_to teacher_user_path(user_tag.user)
+      end
+    end
+  end
+
+  private
+
+  def user_tag_params
+    params.require(:user_tag).permit(:tag_id, :user_id)
+  end
 end
