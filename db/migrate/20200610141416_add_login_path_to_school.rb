ca26846c2e9df29ca9567ffda3de16646b60aff5
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-11 22:48:14 +0900

    Migrationファイルの名前を実態に合うように変更

--- db/migrate/20200610141416_add_login_path_to_school.rb
+++ db/migrate/20200610141416_add_login_path_to_school.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddUidToSchool < ActiveRecord::Migration[5.2]
+class AddLoginPathToSchool < ActiveRecord::Migration[5.2]
   def change
     add_column :schools, :login_path, :string, index: true, after: :id
   end
