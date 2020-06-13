7b97a3314af5d0c6d1c102839f7cc114cffaa902
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-14 00:01:48 +0900

    Schoolsのファクトリーボットを更新

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :school do
     name { Faker::Company.unique.name }
+    login_path { Faker::Internet.unique.domain_word }
   end
 end
