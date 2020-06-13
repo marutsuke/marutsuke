15e90ada12eda9e3d4a6e21d6b01fde915f9c260
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-14 00:02:30 +0900

    Test: セッションコントローラーのテストを修正

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,19 +3,23 @@
 require 'rails_helper'
 RSpec.describe 'SessionsController', type: :request do
   describe '/login#new' do
+    let(:school) { create(:school) }
     it 'ユーザーログイン画面を表示する' do
-      get login_path
+      get school_login_path(school.login_path)
       expect(response).to have_http_status(200)
     end
   end
 
   describe '/login#create ' do
     let(:user) { create(:user) }
-    let(:session_params) { { password: user.password } }
+    let(:session_params) do
+      { password: user.password, school_login_path: user.school.login_path }
+    end
+    let(:another_school) { create(:school) }
 
     it 'メールアドレスでログインできる' do
       session_params[:email] = user.email
-      post login_path, params: { session: session_params }
+      post '/login', params: { session: session_params }
       expect(response).to have_http_status(302)
       follow_redirect!
       expect(response.body).to include("#{user.name}さん")
@@ -30,11 +34,19 @@ RSpec.describe 'SessionsController', type: :request do
       expect(response.body).to include("#{user.name}さん")
       expect(session[:user_id]).to eq user.id
     end
+
+    it '他校の生徒は、ログインできない' do
+      session_params[:login_id] = user.login_id
+      session_params[:school_login_path] = another_school.login_path
+      post login_path, params: { session: session_params }
+      expect(response).to have_http_status(200)
+      expect(session[:user_id]).not_to eq user.id
+    end
   end
 
   describe '/admin/logout#destroy ' do
     let(:user) { create(:user) }
-    let(:session_params) { { login_id: user.login_id, password: user.password } }
+    let(:session_params) { { login_id: user.login_id, password: user.password, school_login_path: user.school.login_path } }
 
     it 'remember meしないでログイン・ログアウトできる' do
       session_params[:remember_me] = '0'
