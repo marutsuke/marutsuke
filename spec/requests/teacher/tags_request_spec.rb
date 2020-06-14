3c73a49587382dff3c4a63732cd3bc79a98ae2b6
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-14 15:58:03 +0900

    Test: tag_newにアクセスできる

--- spec/requests/teacher/tags_request_spec.rb
+++ spec/requests/teacher/tags_request_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Teacher::Tags", type: :request do
+  before { teacher_log_in }
+
+  describe '/teacher/teachers/new#new' do
+    it 'アクセスできる' do
+      get new_teacher_tag_path
+      expect(response).to have_http_status(200)
+    end
+  end
 
 end
