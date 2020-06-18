f2d15499c56b1e008bacc1e5b1d96529f204453d
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-19 00:33:44 +0900

    メイラーのプレビューを見るurlの設定ができた。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { adress: '127.0.0.1', port: 1025 }
-
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
