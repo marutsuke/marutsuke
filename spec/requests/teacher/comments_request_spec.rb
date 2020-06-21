a4fa4cc8accb6253cdc15ed7791c613b0265eae3
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-21 19:43:16 +0900

    Test: コメント追加するrequest_spec

--- spec/requests/teacher/comments_request_spec.rb
+++ spec/requests/teacher/comments_request_spec.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Teacher::Comments", type: :request do
+RSpec.describe 'Teacher::Comments', type: :request do
+  let(:teacher) { create(:teacher) }
+  before { teacher_log_in(teacher) }
 
+  describe '/teacher/comments#create' do
+    let(:answer) { create(:answer) }
+    let(:comment_params) do
+      {
+        text: 'aaaaaaaaaaaaaa',
+        image: '',
+        answer_id: answer.id
+      }
+    end
+    it 'コメントを追加できる' do
+      expect do
+        post teacher_comments_path, params: { comment: comment_params }
+      end.to change(Comment, :count).by(1)
+      expect(response).to have_http_status 302
+      expect(response).to redirect_to teacher_question_path(answer.question)
+    end
+  end
 end
