6d79279861cef98158dc57790fbd8797acf0ac89
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-24 02:14:09 +0900

    Feat: 本番環境のsend_gridの設定が終わった(?)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: Rails.application.credentials.sendgrid[:username],
+    password: Rails.application.credentials.sendgrid[:username],
+    domain: 'em5170.marutsukeapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
