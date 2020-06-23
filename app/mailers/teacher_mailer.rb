943f96549ec04f72937556650d9344052dc9e3d8
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-24 02:18:25 +0900

    Feat: default送信元メールアドレスを設定してみた。

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class TeacherMailer < ApplicationMailer
+  default from: 'test@test.mail.co.jp'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
