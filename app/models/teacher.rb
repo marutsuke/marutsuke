f5bb2a4f90b9912ae25a68acb7b5c30e0f30fc83
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-27 00:19:03 +0900

    Feat: 教員が、update時にpasswordのバリデーションをスキップするように

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,7 +6,8 @@ class Teacher < ApplicationRecord
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 12 }
   validates :email, presence: true, length: { maximum: 50 }, format: { with: VALIDATE_FORMAT_OF_EMAIL }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password, presence: true, length: { minimum: 6 }, on: :update, allow_blank: true
   has_secure_password
   belongs_to :school
   has_many :lessons, dependent: :destroy
