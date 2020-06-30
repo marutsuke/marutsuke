6eb2d0d5323e5a1a05673e5ab88d3d966fbdc819
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-01 00:07:06 +0900

    Test: spec_of_#checking_question_count

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -79,4 +79,39 @@ RSpec.describe Lesson, type: :model do
       end
     end
   end
+
+  describe '#checking_question_count' do
+    subject { lesson.checking_question_count }
+
+    let!(:lesson) { create(:lesson) }
+    let!(:question_1) { create(:question, lesson: lesson) }
+    let!(:question_2) { create(:question, lesson: lesson) }
+    let!(:question_3) { create(:question) }
+    let!(:question_status_1_not) do
+      create(:question_status, :not_submitted, question: question_1)
+    end
+    let!(:question_status_1_checking) do
+      create(:question_status, :checking, question: question_1)
+    end
+    let!(:question_status_2_checking) do
+      create(:question_status, :checking, question: question_2)
+    end
+    context '元々2つチェック中がある' do
+      it { is_expected.to eq 2 }
+    end
+
+    context '別のチェック中のものができると3つになる' do
+      let!(:question_status_2_checking_2) do
+        create(:question_status, :checking, question: question_2)
+      end
+      it { is_expected.to eq 3 }
+    end
+
+    context '別の講座の問題のチェック中のものができても2つ' do
+      let!(:question_status_3_checking) do
+        create(:question_status, :checking, question: question_3)
+      end
+      it { is_expected.to eq 2 }
+    end
+  end
 end
