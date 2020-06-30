ddb565f1f5c09ebfd1080c7f570e610d656a5022
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-06-30 23:19:54 +0900

    Feat: 課題を提出した時に、問題のステータスを確認中に更新する

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,6 +6,7 @@ class AnswersController < ApplicationController
     if @answer.save
       flash[:success] = '課題を提出しました'
       answer_images_save(@answer)
+      question_status_update(@answer.question)
       redirect_to question_path(@answer.question)
     else
       flash[:danger] = '解答の保存に失敗しました。'
@@ -33,6 +34,16 @@ class AnswersController < ApplicationController
       else
         flash[:danger] = '画像の保存に失敗しました。形式やサイズが間違っていないか確かめてください。'
       end
+    end
+  end
+
+  def question_status_update(question)
+    if question_status = current_user.question_statuses.find_by(question_id: question.id)
+      question_status.update(status: :checking)
+    else
+      current_user
+        .question_statuses
+        .new(question_id: question.id, status: :checking).save
     end
   end
 end
