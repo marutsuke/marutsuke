75c582fe7e9efdb782595af15bfdba2a1ec7b6b6
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-01 00:06:47 +0900

    Test: question_statusのステータス別のバリデーションを作成

--- spec/factories/question_statuses.rb
+++ spec/factories/question_statuses.rb
@@ -4,6 +4,10 @@ FactoryBot.define do
   factory :question_status do
     association :user, factory: [:user]
     association :question, factory: [:question]
-    status 10
+    status :not_submitted
+    trait(:not_submitted) { status { :not_submitted } }
+    trait(:checking) { status { :checking } }
+    trait(:submit_again) { status { :submit_again } }
+    trait(:complete) { status { :complete } }
   end
 end
