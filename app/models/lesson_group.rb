706557be857e812464d624338bc8547d50a07cec
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-04 20:30:42 +0900

    Feat: lesson_group周りの関連づけ

--- app/models/lesson_group.rb
+++ app/models/lesson_group.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class LessonGroup < ApplicationRecord
+  belongs_to :school_building
 end
--- app/models/school_building.rb
+++ app/models/school_building.rb
@@ -6,6 +6,7 @@ class SchoolBuilding < ApplicationRecord
   has_many :users, through: :school_building_users
   has_many :school_building_teachers
   has_many :teachers, through: :school_building_teachers
+  has_many :lesson_groups
   validates :name, presence: true, length: { maximum: 20 }
   validates :name, uniqueness: { scope: :school_id }
 end
