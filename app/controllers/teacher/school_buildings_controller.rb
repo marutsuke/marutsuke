1ca92941cd19ccceefb5b665b4379c25192ecfcc
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-04 21:45:20 +0900

    feat: 校舎作成画面の簡易なもの作成

--- app/controllers/teacher/school_buildings_controller.rb
+++ app/controllers/teacher/school_buildings_controller.rb
@@ -8,4 +8,20 @@ class Teacher::SchoolBuildingsController < Teacher::Base
   def new
     @school_building = SchoolBuilding.new
   end
+
+  def create
+    @school_building = current_school.school_buildings.new(school_building_params)
+    if @school_building.save
+      flash[:success] = "#{@school_building.name}を作成しました"
+      redirect_to new_teacher_school_building_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def school_building_params
+    params.require(:school_building).permit(:name)
+  end
 end
