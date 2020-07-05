c530d4acfd3095655f9a918097279eb88b787d30
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-06 01:23:59 +0900

    Test: スコープのテスト

--- spec/models/lesson_group_spec.rb
+++ spec/models/lesson_group_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe LessonGroup, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_most(30) }
 
+  describe 'scope' do
+    describe '#for_school_buildings_belonged_to' do
+      subject { LessonGroup.for_school_buildings_belonged_to(teacher) }
+      let(:teacher) { create(:teacher) }
+      let(:school_building) { create(:school_building) }
+      let(:lesson_group) do
+        create(:lesson_group, school_building: school_building)
+      end
+      context '所属校でない講座は含まない' do
+        it { is_expected.not_to include(lesson_group) }
+      end
+      context '所属校の講座は含む' do
+        let!(:school_building_teacher) do
+          create(:school_building_teacher, teacher: teacher, school_building: school_building)
+        end
+        it { is_expected.to include(lesson_group) }
+      end
+    end
+  end
 end
