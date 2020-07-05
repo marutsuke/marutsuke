4a68fae0afaa103c133720fc2c73958990db7af8
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-06 01:02:44 +0900

    feat: 講座のバリデーションテスト

--- app/models/lesson_group.rb
+++ app/models/lesson_group.rb
@@ -6,6 +6,8 @@ class LessonGroup < ApplicationRecord
   has_many :users, through: :lesson_group_users
   has_many :lessons
 
+  validates :name, presence: true, length: { maximum: 30 }
+
   scope :for_school_buildings_belonged_to, -> (teacher){
     where(school_building_id: teacher.school_building_teachers.pluck(:school_building_id))
   }
--- spec/models/lesson_group_spec.rb
+++ spec/models/lesson_group_spec.rb
@@ -9,4 +9,7 @@ RSpec.describe LessonGroup, type: :model do
     it { is_expected.to have_many(:lesson_group_users) }
     it { is_expected.to have_many(:users) }
   end
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_most(30) }
+
 end
