ca91531892be5830bbf0bc4dcafabd2f9f155d3e
key0123456789 <k.i-0123456789@live.jp>
2020-07-05 14:37:50 +0900

    method definition

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,4 +43,14 @@ module SessionsHelper
   def current_school
     current_user&.school
   end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
 end
