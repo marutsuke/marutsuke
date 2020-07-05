397604a2764f80a61a76a1dbaca2c24c12bc4d58
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-06 00:59:07 +0900

    test: lesson_groupのアソシエーションテスト

--- spec/models/lesson_group_spec.rb
+++ spec/models/lesson_group_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe LessonGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:school_building) }
+    it { is_expected.to have_many(:lessons) }
+    it { is_expected.to have_many(:lesson_group_users) }
+    it { is_expected.to have_many(:users) }
+  end
 end
