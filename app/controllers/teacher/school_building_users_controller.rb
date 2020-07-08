0c2e05058651ea69cc3ac4bd32d2e69c5f3c42c8
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-09 00:33:47 +0900

    feat: 生徒に所属校、兼務校を設定できるように。

---
+++ app/controllers/teacher/school_building_users_controller.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class Teacher::SchoolBuildingUsersController < Teacher::Base
+  before_action :set_user
+  before_action :main_setting, only: :create
+  def new
+    @school_building_user = @user.school_building_users.new
+  end
+
+  def create
+    @school_building_user =
+      @user
+      .school_building_users
+      .new(school_building_user_params)
+    if @school_building_user.save
+      flash[:success] = '登録に成功しました。'
+      redirect_to new_teacher_user_school_building_user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @school_building_user = @user.school_building_users.find(params[:id])
+    @school_building_user.destroy
+    flash[:danger] = '削除に成功しました。'
+    redirect_to new_teacher_user_school_building_user_path(@user)
+  end
+
+  private
+
+  def school_building_user_params
+    params.require(:school_building_user).permit(:school_building_id, :main)
+  end
+
+  def main_setting
+    if school_building_user_params[:main] == '1'
+      @user.school_building_users.update_all(main: false)
+    else
+      school_building_user_params[:main] = false
+    end
+  end
+
+  def set_user
+    @user = current_school.users.find(params[:user_id])
+  end
+end
--- app/models/school_building_user.rb
+++ app/models/school_building_user.rb
@@ -3,4 +3,8 @@
 class SchoolBuildingUser < ApplicationRecord
   belongs_to :user
   belongs_to :school_building
+
+  scope :main_order, lambda {
+    order(main: 'desc')
+  }
 end
