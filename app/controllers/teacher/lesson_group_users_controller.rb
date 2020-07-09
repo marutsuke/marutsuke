3c3f0c1fee0eed792cc21a040ee053598cf16580
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-09 23:07:04 +0900

    Feat: ユーザーの受講講座を登録できるように。

--- app/controllers/teacher/lesson_group_users_controller.rb
+++ app/controllers/teacher/lesson_group_users_controller.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class Teacher::LessonGroupUsersController < Teacher::Base
   before_action :set_user
 
   def new
     @lesson_group_user = @user.lesson_group_users.new
+    @lesson_groups = LessonGroup.for_school_buildings_belonged_to(current_teacher) - @user.lesson_groups
   end
 
   def create
@@ -10,12 +13,18 @@ class Teacher::LessonGroupUsersController < Teacher::Base
       @user
       .lesson_group_users
       .new(lesson_group_user_params)
+    if @lesson_group_user.save
+      flash[:success] = '登録に成功しました。'
+      redirect_to new_teacher_user_lesson_group_user_path(@user)
+    else
+      render :new
+    end
   end
 
   private
 
   def lesson_group_user_params
-    pramas.require(:lesson_group_user).permit(:user_id, :lesson_group_user_id)
+    params.require(:lesson_group_user).permit(:lesson_group_id)
   end
 
   def set_user
