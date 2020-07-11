035ab52975a13bd55acfaa873b015302c33136a1
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-11 20:41:34 +0900

    Feat: N+1の改善

--- app/controllers/teacher/users_controller.rb
+++ app/controllers/teacher/users_controller.rb
@@ -2,7 +2,9 @@
 
 class Teacher::UsersController < Teacher::Base
   def index
-    @users = current_school.users
+    @users = current_school
+             .users
+             .includes(:lesson_groups, lesson_group_users: [:lesson_group])
   end
 
   def new
