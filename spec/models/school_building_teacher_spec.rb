bd9a21da1d4bcc859b07d667b427d845b8d59cab
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-12 22:59:39 +0900

    Test: school_building_teacher

--- spec/models/school_building_teacher_spec.rb
+++ spec/models/school_building_teacher_spec.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SchoolBuildingTeacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { is_expected.to belong_to(:teacher) }
+    it { is_expected.to belong_to(:school_building) }
+  end
+  describe 'teacher_unique_scope_school_building' do
+    let(:teacher) { create(:teacher) }
+    let(:school_building) { create(:school_building) }
+    let(:another_school_building) { create(:school_building) }
+    let!(:school_building_teacher1) do
+      create(:school_building_teacher, teacher: teacher, school_building: school_building)
+    end
+
+    it '同じユーザーで、同じ校舎は、ダメ' do
+      school_building_teacher2 = build(
+        :school_building_teacher, teacher: teacher, school_building: school_building
+      )
+      expect(school_building_teacher2).to_not be_valid
+    end
+    it '同じユーザーで、違う校舎は、OK' do
+      school_building_teacher2 = build(
+        :school_building_teacher, teacher: teacher, school_building: another_school_building
+      )
+      expect(school_building_teacher2).to be_valid
+    end
+  end
 end
