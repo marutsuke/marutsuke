e16a1db28aab3a302de133b3fe53f69f29b3057e
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-07-12 22:58:34 +0900

    Test: school_building_user

--- spec/models/school_building_user_spec.rb
+++ spec/models/school_building_user_spec.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SchoolBuildingUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:school_building) }
+  end
+  describe 'user_unique_scope_school_building' do
+    let(:user) { create(:user) }
+    let(:school_building) { create(:school_building) }
+    let(:another_school_building) { create(:school_building) }
+    let!(:school_building_user1) do
+      create(:school_building_user, user: user, school_building: school_building)
+    end
+
+    it '同じユーザーで、同じ校舎は、ダメ' do
+      school_building_user2 = build(
+        :school_building_user, user: user, school_building: school_building
+      )
+      expect(school_building_user2).to_not be_valid
+    end
+    it '同じユーザーで、違う校舎は、OK' do
+      school_building_user2 = build(
+        :school_building_user, user: user, school_building: another_school_building
+      )
+      expect(school_building_user2).to be_valid
+    end
+  end
 end
