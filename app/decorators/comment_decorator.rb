ff58d769a015da04a91e6e0396f0d34f4e93660b
key0123456789 <k.i-0123456789@live.jp>
2020-07-17 02:09:31 +0900

    create comment decorator

---
+++ app/decorators/comment_decorator.rb
@@ -0,0 +1,15 @@
+class CommentDecorator < Draper::Decorator
+  delegate_all
+
+  def updated_at
+    super&.strftime('%F-%R')
+  end
+
+  def commented_at
+    time_format(updated_at)
+  end
+
+  def time_format(time)
+    Time.parse(time).strftime("%-m/%-d(#{I18n.t('date.abbr_day_names')[Time.parse(time).wday]}) %R")
+  end
+end
