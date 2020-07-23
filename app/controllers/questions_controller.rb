306d7e2e6a86817817f6fff41f5be73e3276be43
key0123456789 <k.i-0123456789@live.jp>
2020-07-23 19:56:23 +0900

    Update questions_controller.rb
    解答の日付で降順にした。

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,6 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @lesson = current_school&.lessons&.find(@question.lesson_id)
-    @answers = current_user&.answers&.where(id: @question.answer_ids)
+    @answers = current_user&.answers&.where(id: @question.answer_ids).order(created_at: "DESC")
   end
 end
