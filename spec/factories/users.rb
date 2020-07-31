9c6d2a19732091e4edf1e79511320d911dcbda07
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-01 02:44:14 +0900

    Test: userファクトリーを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,5 +9,13 @@ FactoryBot.define do
     start_at { 1.day.ago }
     end_at { 1.year.since }
     activated {  true }
+    after(:create) do |user|
+      school = create(:school)
+      school_2 = create(:school)
+      school_user = create(:school_user, school: school, user: user)
+      school_user_2 = create(:school_user, school: school_2, user: user)
+      school_building = create(:school_building, school: school)
+      school_building_user = create(:school_building_user, user: user, school_building: school_building, main: true)
+    end
   end
 end
