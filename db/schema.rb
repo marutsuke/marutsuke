f6eaa57a39e261ad61aadb8ef4fc02572b1621d1
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-01 19:51:18 +0900

    Feat: school_usersにemailを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_093358) do
+ActiveRecord::Schema.define(version: 2020_08_01_103159) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_093358) do
   end
 
   create_table "school_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email"
     t.bigint "school_id"
     t.bigint "user_id"
     t.string "nickname"
