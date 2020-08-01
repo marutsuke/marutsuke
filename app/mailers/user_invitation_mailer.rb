e868c8bf640daf6aaf5795453ff31e985ad46b11
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-01 21:44:35 +0900

    Feat: 新規のユーザーへの招待メール

--- app/mailers/user_invitation_mailer.rb
+++ app/mailers/user_invitation_mailer.rb
@@ -1,12 +1,12 @@
 class UserInvitationMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_invitation_mailer.user_invitation.subject
-  #
   def user_invitation(school_user)
     @school_user = school_user
     mail to: school_user.email, subject: "[Marutsuke]#{school.name}への招待メール"
+  end
+
+  def new_user_invitation(school_user)
+    @school_user = school_user
+    mail to: school_user.email, subject: "[Marutsuke]新規登録のご案内と#{school.name}への招待メール"
   end
 end
