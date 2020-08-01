23c81ae010e2418e5cb7fe8c23826826ab35cb11
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-01 21:53:14 +0900

    fix: バグ修正

--- app/mailers/user_invitation_mailer.rb
+++ app/mailers/user_invitation_mailer.rb
@@ -2,11 +2,11 @@ class UserInvitationMailer < ApplicationMailer
 
   def user_invitation(school_user)
     @school_user = school_user
-    mail to: school_user.email, subject: "[Marutsuke]#{school.name}への招待メール"
+    mail to: school_user.email, subject: "[Marutsuke]#{school_user.school.name}への招待メール"
   end
 
   def new_user_invitation(school_user)
     @school_user = school_user
-    mail to: school_user.email, subject: "[Marutsuke]新規登録のご案内と#{school.name}への招待メール"
+    mail to: school_user.email, subject: "[Marutsuke]新規登録のご案内と#{school_user.school.name}への招待メール"
   end
 end
