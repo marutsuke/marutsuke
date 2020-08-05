a43e6bd4d66db558d4a81f7a32b31dedd0646c58
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-05 23:19:48 +0900

    Test: 所属校を追加する画面のテストと削除するテストを追加

--- spec/requests/teacher/school_building_users_request_spec.rb
+++ spec/requests/teacher/school_building_users_request_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe "Teacher::SchoolBuildingUsers", type: :request do
   let!(:another_school_building_user) { create(:school_building_user, school_building: another_school_building, user: user, main: true) }
   before { teacher_log_in teacher }
 
+  describe '	/teacher/users/:user_id/school_building_users/new#new' do
+    context 'アクセスできる' do
+      it 'アクセスできる' do
+        get new_teacher_user_school_building_user_path(user_id: user.id)
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
   describe '/teacher/users/:user_id/school_building_users#create' do
     context 'main所属校を追加するときは、他の学校は所属ではなくなる' do
       let(:school_building_user_params) do
@@ -37,6 +46,14 @@ RSpec.describe "Teacher::SchoolBuildingUsers", type: :request do
         end.to change(SchoolBuildingUser, :count).by(1)
         expect(another_school_building_user.reload.main).to be true
       end
+    end
+  end
+
+  describe '/teacher/users/:user_id/school_building_users/:id' do
+    it '削除できる' do
+      expect do
+        delete teacher_user_school_building_user_path(another_school_building_user.id, user_id: user.id)
+      end.to change(SchoolBuildingUser, :count).by(-1)
     end
   end
 end
