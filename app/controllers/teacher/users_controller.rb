7b37eb6050867bfa8d91bd80abfd68a75e5b45a0
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-05 22:25:37 +0900

    Feat:生徒一覧画面で、不要な項目を削除

--- app/controllers/teacher/users_controller.rb
+++ app/controllers/teacher/users_controller.rb
@@ -3,8 +3,8 @@
 class Teacher::UsersController < Teacher::Base
   def index
     @users = current_school
-             .users
-             .includes(:lesson_groups, lesson_group_users: [:lesson_group])
+              .users
+              .includes(:lesson_groups, lesson_group_users: [:lesson_group])
   end
 
   def show
