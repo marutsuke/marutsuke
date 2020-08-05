13f53fab1f8ce77c81d0a8ecb098d3c061a44ea4
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-05 22:25:59 +0900

    test: user一覧画面のユーザーがいる時といないときの表示を分けた

--- spec/requests/teacher/users_request_spec.rb
+++ spec/requests/teacher/users_request_spec.rb
@@ -3,9 +3,26 @@
 require 'rails_helper'
 
 describe Teacher::UsersController, type: :request do
-  let!(:teacher) { create(:teacher) }
+  let!(:teacher) { create(:teacher, school: school) }
   before { teacher_log_in teacher }
 
+  describe 'teacher/users#index' do
+    context 'userがいる時' do
+      let!(:user) { create(:user) }
+      let!(:school) { user.schools.first }
+      it 'user一覧を表示' do
+        get teacher_users_path
+        expect(response).to have_http_status(200)
+      end
+    end
+    context 'userがいない時' do
+      let!(:school) { create(:school) }
+      it 'user一覧を表示' do
+        get teacher_users_path
+        expect(response.body).to include('生徒はまだいません')
+      end
+    end
+  end
 
 
 end
