f56340e888e9f943a964f99d01af2d86928c3863
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-08 14:09:38 +0900

    feat: ログイン情報を覚えられないバグを修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,16 +35,18 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by(id: user_id)
+      school_id = cookies.signed[:school_id]
+      school = user&.schools&.find_by(id: school_id)
       if user&.authenticated?(cookies[:user_remember_token])
-        user_log_in(user)
+        user_log_in(user, school)
         @current_user = user
       end
     end
   end
 
   def current_school
-    current_user.schools.find_by(id: cookies.signed[:school_id])
-  end
+    @current_school = current_user.schools.find_by(id: cookies.signed[:school_id])
+    end
 
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
