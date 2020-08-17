1369f9e9403fdaa72c7cfb02c8c160cfaa7067cb
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-08-17 19:23:52 +0900

    Feat:  トップページに不要なものを削除

--- app/controllers/teacher/top_controller.rb
+++ app/controllers/teacher/top_controller.rb
@@ -1,7 +1,5 @@
 class Teacher::TopController < Teacher::Base
   def top
     @lessons_to_check = current_teacher.lessons.to_check
-    @lessons = current_teacher_school
-               .lessons.includes(:teacher)
   end
 end
