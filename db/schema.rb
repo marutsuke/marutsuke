b1f40dacd5e865c19a09bb6042b1ff750fe31ff3
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-09-02 08:23:49 +0900

    feat: line_user_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_151322) do
+ActiveRecord::Schema.define(version: 2020_09_01_231528) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_151322) do
     t.datetime "updated_at", null: false
     t.string "remember_digest"
     t.string "line_state_digest"
+    t.string "line_user_id"
     t.index ["email"], name: "index_users_on_email"
   end
 
