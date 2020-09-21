2345a07060ccb929787f61071699e1de7e4b3b5d
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-09-21 16:18:30 +0900

    Feat: 公開済みの課題がある授業のみトップページに表示

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,7 +4,11 @@ class LessonsController < UserBase
   before_action :set_lessons_scope, only: [:index]
   def index
     lesson_group_ids = current_user.lesson_groups.for_school(current_school).pluck(:id)
-    @new_lessons = current_school.lessons.where(lesson_group_id: lesson_group_ids).includes(:teacher)
+    @new_lessons = current_school
+      .lessons
+      .where(lesson_group_id: lesson_group_ids)
+      .includes(:teacher)
+      .has_published_question
   end
 
   def show
