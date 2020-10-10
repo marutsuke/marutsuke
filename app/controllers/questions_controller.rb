f44cf90d4e1cfd421c1f40d1f8cc0ab4b0dddb13
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-10-10 14:53:09 +0900

    feat: qeustions/showの下のボタンの追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@
 class QuestionsController < UserBase
   def show
     @question = Question.find(params[:id])
+    @status = @question.question_status_of(current_user).status
     @lesson = current_school&.lessons&.find(@question.lesson_id)
     @answers = current_user&.answers&.where(id: @question.answer_ids).order(created_at: "DESC")
   end
