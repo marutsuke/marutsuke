8740b9e27a798f1a2a1e3f70cc671ba1e5dac42e
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-10-18 02:08:07 +0900

    feat: 学校に絞ったスコープを追加

--- app/models/lesson_group_user.rb
+++ app/models/lesson_group_user.rb
@@ -1,4 +1,8 @@
 class LessonGroupUser < ApplicationRecord
   belongs_to :user
   belongs_to :lesson_group
+
+  scope :for_school, lambda { |school|
+    joins(:lesson_group).merge(LessonGroup.for_school(school))
+  }
 end
--- spec/models/lesson_group_user_spec.rb
+++ spec/models/lesson_group_user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe LessonGroupUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#for_school' do
+    subject { LessonGroupUser.for_school(school) }
+    let(:user) { create(:user) }
+    let(:school_building) { create(:school_building) }
+    let(:school) { school_building.school }
+    let(:lesson_group) do
+      create(:lesson_group, school_building: school_building)
+    end
+    let(:lesson_group_user) { create(:lesson_group_user, lesson_group: lesson_group, user: user) }
+    let(:other_school) { create(:school) }
+    let(:other_school_building) { create(:school_building, school: other_school) }
+    let(:other_lesson_group) do
+      create(:lesson_group, school_building: other_school_building)
+    end
+    let(:other_lesson_group_user) { create(:lesson_group_user, lesson_group: other_lesson_group, user: user) }
+
+    context '学校の講座は含む' do
+      it { is_expected.to include(lesson_group_user) }
+    end
+
+    context '学校のものでない講座は含まない' do
+      it { is_expected.not_to include(other_lesson_group_user) }
+    end
+  end
 end
