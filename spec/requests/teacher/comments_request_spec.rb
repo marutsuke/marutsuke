1e9e9b64f4e59e30cbeb51cedb5613c314e25702
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-11-03 13:01:39 +0900

    Test: コメントのテスト追加

--- spec/requests/teacher/comments_request_spec.rb
+++ spec/requests/teacher/comments_request_spec.rb
@@ -3,11 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Teacher::Comments', type: :request do
-  let(:teacher) { create(:teacher) }
+  let(:teacher) { create(:teacher, school: question.lesson.school) }
+  let(:question) { question_status.question }
+  let!(:question_status) { create(:question_status, :checking) }
+  let!(:answer) { create(:answer, question: question, user: question_status.user) }
   before { teacher_log_in(teacher) }
 
-  describe '/teacher/comments#create' do
-    let(:answer) { create(:answer) }
+  describe '/teacher/question_statuses/:question_status_id/comments/new#new ' do
+    it 'アクセスできる' do
+      get new_teacher_question_status_comment_path(question_status)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+
+  describe '/teacher/question_statuses/:question_status_id/comments#create' do
     let(:comment_params) do
       {
         text: 'aaaaaaaaaaaaaa',
@@ -15,12 +25,24 @@ RSpec.describe 'Teacher::Comments', type: :request do
         answer_id: answer.id
       }
     end
-    it 'コメントを追加できる' do
-      expect do
-        post teacher_comments_path, params: { comment: comment_params }
-      end.to change(Comment, :count).by(1)
-      expect(response).to have_http_status 302
-      expect(response).to redirect_to teacher_question_path(answer.question)
+    context "validation通過時" do
+      it 'コメントを追加できる' do
+        expect do
+          post teacher_question_status_comments_path(question_status), params: { comment: comment_params }
+        end.to change(Comment, :count).by(1)
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to new_teacher_question_status_comment_path(question_status)
+      end
+    end
+    context 'validationに引っかかる時' do
+      before { comment_params[:text] = '' }
+      it 'エラーが表示される' do
+        expect do
+          post teacher_question_status_comments_path(question_status), params: { comment: comment_params }
+        end.to change(Comment, :count).by(0)
+        expect(response).to have_http_status 200
+        expect(response.body).to include('入力してください')
+      end
     end
   end
 end
