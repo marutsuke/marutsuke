c8acdc9ce955bd2ccd72d25b3395f6dbbaba2b62
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-11-15 23:00:16 +0900

    feat: join_requestを作成したので、不要なものを削除

--- app/controllers/school_users_controller.rb
+++ app/controllers/school_users_controller.rb
@@ -5,28 +5,6 @@ class SchoolUsersController < UserBase
     @school_users = current_user.school_users
   end
 
-  def new
-    @school_user =  current_user.school_users.new
-  end
-
-  def create
-    @school_building = SchoolBuilding.find_by(invitation_code: params[:invitation_code]) if params[:invitation_code]
-
-    if @school_building
-        if @school_building.invite_user(current_user)
-          flash[:success] = "#{ @school_building.school.name }/#{ @school_building.name }に入学リクエストを送りました。"
-          redirect_to new_school_user_path
-        else
-          flash[:danger] = '既に所属している学校です。'
-          redirect_to new_school_user_path
-        end
-    else
-      @school_user =  current_user.school_users.new
-      flash[:danger] = '招待コートが間違っているようです。'
-      render :new
-    end
-  end
-
   def select_school
     @school_user = current_user.school_users.find(params[:id])
     if @school_user.activated
--- app/models/school_building.rb
+++ app/models/school_building.rb
@@ -14,12 +14,4 @@ class SchoolBuilding < ApplicationRecord
   validates :name, uniqueness: { scope: :school_id, case_sensitive: true }
   validates :invitation_code, uniqueness: true
 
-  def invite_user(user)
-    if user.school_users.pluck(:school_id).include?(school_id)
-      return false
-    else
-      user.school_users.create(school_id: school.id, school_building_id: id)
-      user.school_building_users.create(school_building_id: id, main: true)
-    end
-  end
 end
