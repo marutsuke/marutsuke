e9de57f98b8f6e4738e214b5e9346af04828ff38
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-11-15 16:09:37 +0900

    feat: 不要なviewの削除など

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,8 @@
 class UsersController < UserBase
   skip_before_action :user_login_required,
                      :school_select_required,
-                     only: %i[new
-                              mypage
-                              new_authentication_form_by_email
-                              create_user_authentication_by_email
-                            ]
-  before_action :user_log_out_required,
-                only: %i[new
-                         new_authentication_form_by_email
-                         create_user_authentication_by_email
-                        ]
+                     only: %i[new mypage]
+  before_action :user_log_out_required, only: %i[new]
 
   def mypage; end
 
@@ -37,50 +29,10 @@ class UsersController < UserBase
   end
 
   def new
-    @user = User.new
-  end
-
-  def new_authentication_form_by_email
-    @user_authentication = UserAuthentication.new
-  end
-
-  def create_user_authentication_by_email
-    @user_authentication = UserAuthentication.new(user_authentication_params)
-    @user_authentication.provider = 'email'
-
-    if @old_user_authentication = UserAuthentication.find_by(provider: 'email', uid: @user_authentication.uid)
-      if @old_user_authentication.user_id.present?
-        flash[:danger] = '入力されたメールアドレスは、既に登録されています。ログインしてください。'
-        redirect_to new_user_path
-      else
-        @old_user_authentication.authentication_token_save
-        @old_user_authentication.send_activation_mail
-        flash[:success] = '入力頂いたメールアドレスにメールを送りました。メールを確認してください。'
-        redirect_to new_authentication_form_by_email_users_path
-      end
-    else
-      if @user_authentication.save
-        @user_authentication.authentication_token_save
-        @user_authentication.send_activation_mail
-        flash[:success] = '入力頂いたメールアドレスにメールを送りました。メールを確認してください。'
-        redirect_to new_authentication_form_by_email_users_path
-      else
-        flash[:danger] = 'エラー：入力頂いたメールアドレスにメールを送れませんでした。'
-        redirect_to new_authentication_form_by_email_users_path
-      end
-    end
   end
 
   private
   def user_params
     params.require(:user).permit(:image, :name, :name_kana, :email, :birth_day, :school_grade)
-  end
-
-  def user_authentication_params
-    params.require(:user_authentication).permit(:uid)
-  end
-
-  def email_already_exist(email)
-    UserAuthentication.find_by(uid: email, provider: 'email')
   end
 end
