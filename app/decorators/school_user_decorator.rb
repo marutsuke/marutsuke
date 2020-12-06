9c94a9cfc7ef75a5f54fb008d60ab6d025b5607b
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-06 16:09:27 +0900

    feat: アカウント開始と終了を示した

--- app/decorators/school_user_decorator.rb
+++ app/decorators/school_user_decorator.rb
@@ -10,4 +10,12 @@ class SchoolUserDecorator < Draper::Decorator
   def activated_at
     super&.strftime('%F-%R') || '未設定'
   end
+
+  def start_at
+    super&.strftime('%F-%R') || '未設定'
+  end
+
+  def end_at
+    super&.strftime('%F-%R') || '未設定'
+  end
 end
