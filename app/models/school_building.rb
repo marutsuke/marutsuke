bf05ca3bada8fa0c40c4cd01843819d9eaa77842
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-12 14:39:04 +0900

    feat: nullで被って許可出してしまうので調整

--- app/models/school_building.rb
+++ app/models/school_building.rb
@@ -12,6 +12,6 @@ class SchoolBuilding < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :name, uniqueness: { scope: :school_id, case_sensitive: true }
-  validates :invitation_code, uniqueness: { case_sensitive: true }
+  validates :invitation_code, uniqueness: { case_sensitive: true }, length: { minimum: 8 }, allow_blank: true
 
 end
