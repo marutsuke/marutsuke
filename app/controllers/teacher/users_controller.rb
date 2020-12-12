215ae45b91e41c9b3a23b74004af7a92d282153c
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-12 15:05:02 +0900

    feat: 検索条件を校舎から学年に変更(校舎の生徒しか表示しないようにしたから)

--- app/controllers/teacher/users_controller.rb
+++ app/controllers/teacher/users_controller.rb
@@ -26,12 +26,12 @@ class Teacher::UsersController < Teacher::Base
   private
 
   def search_users
-    @q = current_teacher_school.users.ransack(params[:q])
+    @q = current_teacher_school_building.users.ransack(params[:q])
     @users = @q.result(distinct: true)
     # セレクトボックスの初期値設定のため
     if params[:q]
       @lesson_group_id = params[:q][:lesson_group_users_lesson_group_id_eq]
-      @school_building_id = params[:q][:school_building_users_school_building_id_eq]
+      @school_grade = params[:q][:school_grade_eq]
     end
   end
 
