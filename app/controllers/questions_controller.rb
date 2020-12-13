8000b82aa7857fa61f890de5b284151b9dc1a2e2
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-13 19:30:33 +0900

    Feat: コメントあり、なしを判断して、既読コメントかどうかをつけるように

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 class QuestionsController < UserBase
+  after_action :comment_check, only: [:show]
+
   def show
     @question = Question.find(params[:id])
-    @status = @question.question_status_of(current_user).status
+    @question_status = @question.question_status_of(current_user)
+    @status = @question_status.status
     @lesson = current_school&.lessons&.find(@question.lesson_id)
     @answers = current_user&.answers&.where(id: @question.answer_ids).order(created_at: "ASC")
   end
+
+  private
+  def comment_check
+    if @status == 'commented'
+      @question_status.update(status: :comment_checked)
+    end
+  end
+
 end
