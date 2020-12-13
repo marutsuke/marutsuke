9f1bedec50fd0e00956fe4901ab01d037fdfbf3f
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-13 18:36:57 +0900

    Test: 受講講座のアクセステスト

--- spec/requests/lesson_groups_request_spec.rb
+++ spec/requests/lesson_groups_request_spec.rb
@@ -6,8 +6,15 @@ RSpec.describe "LessonGroups", type: :request do
   before { user_log_in(user, school) }
 
   describe '/lesson_groups#index' do
-    it 'アクセスできるアクセスできるアクセスして' do
+    it 'アクセスできる' do
       get lesson_groups_path
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe '/lesson_groups#show' do
+    let(:lesson_group_user){ create(:lesson_group_user, user: user) }
+    it 'アクセスできる' do
+      get lesson_group_path(lesson_group_user.lesson_group)
       expect(response).to have_http_status(200)
     end
   end
