c1595e6b2754309f30508106421ee0a8f6340dd5
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-16 21:12:10 +0900

    feat: 課題に教員用メモを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_221959) do
+ActiveRecord::Schema.define(version: 2020_12_16_115112) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 2020_12_13_221959) do
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
+    t.text "note"
     t.string "image"
     t.bigint "lesson_id"
     t.datetime "created_at", null: false
