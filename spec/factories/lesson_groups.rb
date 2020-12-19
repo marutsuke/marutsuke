096fe33e0d9ee898546870354b9f209c22843a59
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2020-12-20 06:53:52 +0900

    test: 講座に期間を設けたので、テストの修正。

--- spec/factories/lesson_groups.rb
+++ spec/factories/lesson_groups.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
   factory :lesson_group do
     name { Faker::Name.unique.name[0..10] }
     association :school_building, factory: [:school_building]
+    start_at { 1.day.ago }
+    end_at { 1.year.since }
     min_school_grade { 4 }
     max_school_grade { nil }
     free_attend { false }
