6bf9086f0c4a28bfffbefcca0e36385eb3fff5ad
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2021-01-10 16:47:43 +0900

    Feat: 名前ではなく、ニックネームにした

--- app/controllers/school_user_activations_controller.rb
+++ app/controllers/school_user_activations_controller.rb
@@ -26,7 +26,7 @@ class SchoolUserActivationsController < UserBase
       flash[:danger] = '有効でないリンク、または、すでに参加しています。'
       redirect_to root_path
     else
-      flash[:success] = "ようこそ！名前とパスワードを設定してください。"
+      flash[:success] = "ようこそ！ニックネームとパスワードを設定してください。"
       @new_user_activation_form = NewUserActivationForm.new(@school_user)
     end
   end
