80adf40429964dca0c065f0a816fe0cba3621d15
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2021-01-30 12:06:50 +0900

    feat: capfileのバージョンが変わったぽいので修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid for current version and patch releases of Capistrano
-lock '3.14.1'
+  # config valid for current version and patch releases of Capistrano
+lock '3.15.0'
 
 set :application, "marutsuke"
 set :repo_url, "git@github.com:marutsuke/marutsuke.git"
