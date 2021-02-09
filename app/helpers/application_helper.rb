4bd82bb4eea0cfb088db4422860cf94293b59ffa
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2021-02-09 09:18:01 +0900

    feat: pdfでimageの出しわけをしたい

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  ## pdfの時の出しわけをしたい
+  def image_or_pdf_tag(question, html_class = 'm-question__image')
+    if question.image_url.match(/pdf$/)
+      tag.object '', data: question.image_url, type: 'application/pdf',style: "width: 100%;", class: html_class
+    else
+      image_tag question.image_url, alt: question.image_alt, class: html_class
+    end
+  end
 end
